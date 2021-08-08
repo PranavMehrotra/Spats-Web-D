236939c4b52537bf10c22707c93d49cce75226c0
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2021-08-08 14:11:34 +0530

    Add files via upload

---
+++ Task 3/script.js
@@ -0,0 +1,24 @@
+function wel_func(){
+    document.getElementById("wel_2").style.transform= "scale(0.2)";
+    document.getElementById("wel_1").style.transform= "scale(0.2)";
+    setTimeout(function(){document.getElementById("wel_2").style.display="none";
+        document.getElementById("wel_1").style.display="none";},1400);
+}
+var l=0;
+function lists(){
+    if(l==0){
+        l=1;
+        document.getElementById("list").style.transform="translateX(0%)";
+        document.getElementById("line1").style.transform="rotate(-45deg) translate(-5px,6px)";
+        document.getElementById("line2").style.opacity="0";
+        document.getElementById("line3").style.transform="rotate(45deg) translate(-4px,-4.5px)";
+}
+else {
+    l=0;
+    document.getElementById("list").style.transform="translateX(100%)";
+    document.getElementById("line1").style.transform="rotate(0deg) translate(0,0)";
+    document.getElementById("line2").style.opacity="1";
+    document.getElementById("line3").style.transform="rotate(0deg) translate(0,0)";
+
+}
+}
